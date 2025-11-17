c1d0a4f55e4bdc049442de688048d27839ed2994
Sneha Sharma <your-github-email@example.com>
2025-11-17 20:50:24 +0530

    updated trendora logo text

--- Components/Logo_Name.jsx
+++ Components/Logo_Name.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Logo_Name = () => {
   return (
-    <div className='text-4xl font-bold p-5 mx-3 text-black w-[20%] '>
+    <div className='text-4xl font-bold p-5 mx-3 text-black w-[20%] ml-[3rem] '>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 160" width="500" height="150">
   <defs>
-    <linearGradient id="gradD" x1="0" y1="0" x2="1" y2="0">
+    <linearGradient id="gradD" x1="0" y1="0" x2="4" y2="0">
       <stop offset="0%" stop-color="#FF6A6A"/>
       <stop offset="33%" stop-color="#6Eg6FF"/>
       <stop offset="66%" stop-color="#FFBC73"/>
@@ -16,7 +16,7 @@ const Logo_Name = () => {
   <text x="0" y="100"
         font-family="Montserrat, Poppins, sans-serif"
         font-weight="800"
-        font-size="150"
+        font-size="120"
         fill="url(#gradD)">
     TrendOra
   </text>
