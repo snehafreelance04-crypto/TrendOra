530b5df9c2a186c33e1f4ad3d906e4f5684f4d6e
Sneha Sharma <your-github-email@example.com>
2025-11-19 18:10:46 +0530

    logo fixed

--- Components/Logo_Name.jsx
+++ Components/Logo_Name.jsx
@@ -1,28 +1,55 @@
+"use client"
 import React from 'react'
+import Image from 'next/image'
 
 const Logo_Name = () => {
   return (
-    <div className='text-3xl font-bold p-5 mx-3 text-black w-[20rem] ml-[3rem] '>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 160" width="450" height="150">
-  <defs>
-    <linearGradient id="gradD" x1="0" y1="0" x2="4" y2="0">
-      <stop offset="0%" stop-color="#FF6A6A"/>
-      <stop offset="33%" stop-color="#6Eg6FF"/>
-      <stop offset="66%" stop-color="#FFBC73"/>
-      <stop offset="100%" stop-color="#"/>
-    </linearGradient>
-  </defs>
+    <>
+      <div className='flex items-center font-bold p-5 ml-[1rem] w-auto'>
 
-  <text x="0" y="100"
-        font-family="Montserrat, Poppins, sans-serif"
-        font-weight="800"
-        font-size="120"
-        fill="url(#gradD)">
-    TrendOra
-  </text>
-</svg>
+        {/* Responsive Logo Image Wrapper */}
+        <div className="w-[150px] h-[120px] md:w-[160px] md:h-[120px]">
+          <Image
+            src="/Images/Logo1.png"
+            alt="logo"
+            width={400}
+            height={200}
+            className="w-full h-full object-contain"
+          />
+        </div>
 
-    </div>
+        {/* TrendOra SVG */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 600 120"
+          width="400"
+          height="130"
+          className="ml-2 "
+        >
+          <defs>
+            <linearGradient id="gradD" x1="0" y1="0" x2="4" y2="0">
+              <stop offset="0%" stopColor="#FF6A6A" />
+              <stop offset="33%" stopColor="#6E96FF" />
+              <stop offset="66%" stopColor="#FFBC73" />
+              <stop offset="100%" stopColor="#FF6A6A" />
+            </linearGradient>
+          </defs>
+
+          <text
+            x="0"
+            y="80"
+            fontFamily="Montserrat, Poppins, sans-serif"
+            fontWeight="800"
+            fontSize="100"
+            className="sm:text-[50px] md:text-[110px]"
+            fill="url(#gradD)"
+          >
+            TrendOra
+          </text>
+        </svg>
+
+      </div>
+    </>
   )
 }
 
